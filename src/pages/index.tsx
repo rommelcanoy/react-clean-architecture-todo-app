0dbffd212544d97b3d1520fd592153249df3b169
Rommel Canoy <rommelcanoy4@gmail.com>
2022-03-28 10:30:33 +0800

    removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import { useAppDispatch, useAppSelector } from "../app/redux/hooks"
 import TodoList from 'app/components/Todo/TodoList';
 import TodoInput from '../app/components/Todo/TodoInput';
 import { useEffect, useState } from 'react';
-import db from 'data/repositories/firebase';
-import { collection, onSnapshot } from 'firebase/firestore';
 
 function App() {
   const dispatch = useAppDispatch()
@@ -15,14 +13,6 @@ function App() {
   }))
 
   useEffect(() => {
-    let todos = [];
-    let test = [];
-    // onSnapshot(collection(db, 'todos'), (snapshot) => {
-    //   test = snapshot.docs.map((doc) => doc.data());
-    //   todos = [...test];
-    //   console.log('todos:', todos);
-    // });
-    // dispatch(fetchTodo())
     dispatch(getTodoAsync())
   }, []);
 
@@ -31,9 +21,6 @@ function App() {
       <div className="bg-white p-5 rounded max-w-screen-sm w-full flex flex-col gap-5 text-gray-500">
         <h1 className="text-lg font-medium text-center text-gray-500">{ loading ? <span>Loading...</span> : <span>To Do</span>}</h1>
         <TodoInput />
-        {/* {
-        loading ? <div className="text-sm text-center text-slate-400">Loading...</div> : <TodoList list={todo} />
-        } */}
         <TodoList list={todo} />
       </div>
     </div>
